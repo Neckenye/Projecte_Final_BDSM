88af5ed0b584ce7533c54ee2dd321e900d3954df
Neckenye <danimartineznavarro16@gmail.com>
2025-05-07 17:09:16 +0200

    Can build
    This changes are to prepare a basic build of the game.

--- BdSM_Project/Assets/Scripts/Char_LoseCompare.cs
+++ BdSM_Project/Assets/Scripts/Char_LoseCompare.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Char_WinCompare : MonoBehaviour
+public class Char_LoseCompare : MonoBehaviour
 {    
     // On collision enter to Char, go to next level, and if its a trap, restart level.
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Character"))
+        if (collision.gameObject.CompareTag("Pincho"))
         {
             Debug.Log("Collision Sucess");
-            ChangeScene.instance.AdvanceLevel();
+            ChangeScene.instance.ResetLevel();
         }
     }    
 }
