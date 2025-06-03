6e018e9ac87aba21996cb9f313682ee0c5d9aebb
Neckenye <danimartineznavarro16@gmail.com>
2025-06-03 17:05:31 +0200

    turrets init on press

--- BdSM_Project/Assets/Scripts/Enemy/MustardShoot.cs
+++ BdSM_Project/Assets/Scripts/Enemy/MustardShoot.cs
@@ -21,20 +21,23 @@ public class Turret_Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeBetweenShootsCopy -= Time.deltaTime;
-        if (timeBetweenShootsCopy <= 0)
+        if (Timer.startTime)
         {
-            GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
-            Projectile component = projectile.GetComponent<Projectile>();
-            component._desireDirection.y = projectileDirectionY;
-            component._desireDirection.x = projectileDirectionX;
-            timeBetweenShootsCopy = timeBetweenShoots;
+            timeBetweenShootsCopy -= Time.deltaTime;
+            if (timeBetweenShootsCopy <= 0)
+            {
+                GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+                Projectile component = projectile.GetComponent<Projectile>();
+                component._desireDirection.y = projectileDirectionY;
+                component._desireDirection.x = projectileDirectionX;
+                timeBetweenShootsCopy = timeBetweenShoots;
 
-            //Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-            //Projectile component = projectile.GetComponent<Projectile>();
-            //component._desireDirection.x = 1;
-            //timeBetweenShootsCopy = timeBetweenShoots;
-            //Debug.Log("Spawned projectile");
+                //Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+                //Projectile component = projectile.GetComponent<Projectile>();
+                //component._desireDirection.x = 1;
+                //timeBetweenShootsCopy = timeBetweenShoots;
+                //Debug.Log("Spawned projectile");
+            }
         }
     }
 }
