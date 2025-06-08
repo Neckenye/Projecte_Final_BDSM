27b1ef92ca16cb9e079cd1c1817cacf8f3e9ef9a
MartiBorniquel <mborniquelb@edu.tecnocampus.cat>
2025-06-08 17:42:55 +0200

    Mini pollish

--- BdSM_Project/Assets/Scripts/Structures/MovingPlatform.cs
+++ BdSM_Project/Assets/Scripts/Structures/MovingPlatform.cs
@@ -8,14 +8,12 @@ public class Structures : MonoBehaviour
     [SerializeField] private float velocity;
     private int nextPlatform = 1;
     private bool platformOrder = true;
-    // Start is called before the first frame update
 
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     private void Update()
     {
         if (platformOrder && nextPlatform + 1 >= wayPoints.Length)
